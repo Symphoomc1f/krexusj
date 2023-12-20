a57fb8f0bd9ff7e563a49bc8342c3df15b236a16
Symphoomc1f <gaoyan97714@163.com>
2023-12-20 11:39:14 +0800

    打印日志

--- back/src/main/java/com/java110/things/adapt/accessControl/sxoa/SxCommomFactory.java
+++ back/src/main/java/com/java110/things/adapt/accessControl/sxoa/SxCommomFactory.java
@@ -222,6 +222,8 @@ public class SxCommomFactory {
             logger.error("请求下游服务【" + MappingCacheFactory.getValue("SXOA_URL") + GET_SIGN + "】异常，参数为" + httpEntity + e.getResponseBodyAsString(), e);
             throw e;
         }
+        logger.debug("请求地址："+MappingCacheFactory.getValue("SXOA_URL") + GET_SIGN+"请求信息：" + httpEntity + ",返回参数：" + responseEntity);
+
         if (responseEntity.getStatusCode() != HttpStatus.OK) {
             throw new IllegalStateException("请求sign失败" + responseEntity);
         }
@@ -249,6 +251,8 @@ public class SxCommomFactory {
             logger.error("请求下游服务【" + MappingCacheFactory.getValue("SXOA_URL") + GET_TOKEN + "】异常，参数为" + httpEntity + e.getResponseBodyAsString(), e);
             throw e;
         }
+        logger.debug("请求地址："+MappingCacheFactory.getValue("SXOA_URL") + GET_TOKEN+"请求信息：" + httpEntity + ",返回参数：" + responseEntity);
+
         if (responseEntity.getStatusCode() != HttpStatus.OK) {
             throw new IllegalStateException("请求sign失败" + responseEntity);
         }
